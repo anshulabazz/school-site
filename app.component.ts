bcfc589633b980f95dccda98997fb18fbe197274
anshulabazz <9882843092as@gmail.com>
2021-04-01 15:57:45 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'school-site';
+  myimage: string = "assets/images/anshul.jpg";
+
+  myimage1: string = "assets/images/logo.png";
+  myimage2: string = "assets/images/london.png";
+  myimage3: string = "assets/images/newyork.png";
+  myimage4: string = "assets/images/washington.png";
+  myimage5: string = "assets/images/library.png";
+  myimage6: string = "assets/images/basketball.png";
+  myimage7: string = "assets/images/cafeteria.png";
+
+  myimage8: string = "assets/images/user1.jpg";
+
+  myimage9: string = "assets/images/user2.jpg";
+  myimage10: string = "assets/images/jaihind.jpg";
+  myimage11: string = "assets/images/india.jpg";
+}
